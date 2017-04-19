ce1459a29e001202b8e50d2df6df5e4407cda34a
Molly Zhang <mollyzhang@ucsc.edu>
2017-04-18 18:00:20 -0700

    update startup_script.py'

---
+++ startup_script.py
@@ -0,0 +1,19 @@
+
+# numerical & stats 
+import pandas as pd
+import numpy as np
+from scipy import stats
+
+# plotting
+from matplotlib import pyplot as plt
+from bokeh.charts import Bar, output_file, show
+from bokeh.io import output_notebook
+
+
+# machine learning
+from sklearn import linear_model
+
+# jupyte notebook amenities
+output_notebook()
+%matplotlib inline
+%autosave 10
