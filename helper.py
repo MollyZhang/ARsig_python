6d2b3f21c1b5f9c0a0f40b9b8b94b9fb7ef92357
Molly Zhang <mollyzhang@ucsc.edu>
2017-04-24 18:32:27 -0700

    seperate out leave-pair-out cross validaiton as it's own module

---
+++ helper.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+
+def leave_pair_out_cv(X, Y, classifier):
+    """ Leave pair out cross validation as defined in this paper:
+        http://www.jmlr.org/proceedings/papers/v8/airola10a/airola10a.pdf
+    """
+    pairs = get_all_01_pairs(Y)
+    auc = []
+    for pair in pairs:
+        X_train = X[~X.index.isin(pair)]
+        Y_train = Y[~Y.index.isin(pair)]
+        X_test = X[X.index.isin(pair)]
+        Y_test = Y[Y.index.isin(pair)]
+        assert(list(X_train.index)==list(Y_train.index))
+        assert(list(X_test.index)==list(Y_test.index))        
+        classifier.fit(X_train, Y_train['ligand'])
+        coef = classifier.coef_[0]
+        Y_pred = np.matmul(X_test, coef)        
+        auc.append(calc_auc_cv(Y_pred, Y_test))
+    auc = float(sum(auc))/len(pairs)
+    return auc
+
+
+def get_all_01_pairs(Y):
+    pairs = []
+    label = Y.columns[0]
+    zeros = Y[Y[label]==0].index
+    ones = Y[Y[label]==1].index    
+    for zero in zeros:
+        for one in ones:
+            pairs.append((zero, one))
+    return pairs
+
+
+def calc_auc_cv(Y_pred, Y_test):
+    Y_test = list(Y_test['ligand'])
+    assert(Y_test[1] != Y_test[0])
+    if Y_test[1] > Y_test[0]:
+        zero = Y_pred[0]
+        one = Y_pred[1]
+    else:
+        zero = Y_pred[1]
+        one = Y_pred[0]
+    if zero == one:
+        return 0.5
+    elif zero < one:
+        return 1
+    else:
+        return 0
